c3174f8608d95e619b070f60af55b070c6e95166
schwabochino <philipp.schwarberg@outlook.de>
2017-09-13 23:03:42 +0200

    LTP erstellt

---
+++ LTP/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 20
+
+struct spielerListe
+{
+    char *nickName[64];
+    struct spielerListe *next;
+
+};
+
+int main()
+{
+
+    return 0;
+}
