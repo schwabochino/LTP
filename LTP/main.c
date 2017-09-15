e44fffc632f6a344b0ae00d874f3b8cca9700711
schwabochino <philipp.schwarberg@outlook.de>
2017-09-15 21:03:14 +0200

    Zufallszahl

--- LTP/main.c
+++ LTP/main.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <math.h>
 #define MAX 20
 
 struct spielerListe
 {
-    char *nickName[64];
+    char nickName[256];
     struct spielerListe *next;
 };
 
 int main()
 {
-    printf("kein plan");
+    printf("\t++++LanTurnierPlaner++++\n");
+    int i;
+    int diceRoll;
+        srand(time(NULL));
+
+    /*for(i=0; i<20; i++)
+    {
+            diceRoll = (rand()%6)+1;
+            printf("%d\n", diceRoll);
+
+    }*/
+
     return 0;
 }
