3b0c548b0d7b93b8e45fbf17a224d79ce577ef85
schwabochino <philipp.schwarberg@outlook.de>
2017-09-15 21:27:36 +0200

    Aufgabe 3_2 erledigt

---
+++ 3_2_klodergr/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float zahlEins=0, zahlZwei=0;
+    printf("Geben Sie die erste Zahl ein: ");
+    scanf("%f", &zahlEins);
+    fflush(stdin);
+    printf("Geben Sie die zweite Zahl ein: ");
+    scanf("%f", &zahlZwei);
+    fflush(stdin);
+    if (zahlEins > zahlZwei)
+    {
+        printf("%f ist > %f",zahlEins, zahlZwei);
+    }else if(zahlEins < zahlZwei)
+    {
+        printf("%f ist < %f", zahlEins, zahlZwei);
+    }else
+    {
+        printf("%f ist gleich %f", zahlEins, zahlZwei);
+    }
+    return 0;
+}
